fe363d0a4109c3e349e82cdd43dd62a799bebe75
agata.isabellysouza@gmail.com <agata.isabellysouza@gmail.com>
2024-03-19 08:35:32 -0300

    formulario

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
+
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email"); 
+ 
+var telefone = document.getElementById("telefone"); 
+ 
+var cep = document.getElementById("cep"); 
+
+var logradouro = document.getElementById("logradouro"); 
+ 
+var numero = document.getElementById("numero"); 
+ 
+var complemento = document.getElementById("complemento"); 
+ 
 
 function alertar(){
-     alert( nome.value + " " + "Voce clicou no botão!!!");
+     //alert( nome.value + " " + "Voce clicou no botão!!!")
+     saida.innerText = "Nome: " + " " + nome.value +  
+     "\n email: " + "  "  + email.value;
 }
